06e0d0a7d71ce30de731351099d624ec47374663
Alex De Leon <alex.deleon-bonilla2stu.ocsb.ca>
2024-06-13 13:55:19 -0400

    Click Click 1

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-/**
- * This file will get loaded by index.html
- */
+let clicks = 0;
+
+document.getElementById("btn_click").addEventListener("click", buttonClicked);
+document.getElementById("btn_decrease").addEventListener("click", decreaseClicked);
+document.getElementById("btn_reset").addEventListener("click", resetClicked);
+
+function buttonClicked() {
+    clicks++;
+    updateClickCounter();
+    console.log("The button was clicked");
+}
+
+function decreaseClicked() {
+    clicks--;
+    updateClickCounter();
+    console.log("The decrease button was clicked");
+}
+
+function resetClicked() {
+    clicks = 0;
+    updateClickCounter();
+    console.log("The reset button was clicked");
+}
+
+function updateClickCounter() {
+    document.getElementById("click_counter").textContent = clicks;
+}
